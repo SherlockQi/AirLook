a557161243819bd6ef73d568f2cfaa53bad29774
SherlockQi <xiaqi90@163.com>
2017-12-28 09:49:23 +0800

    update-UI

--- AirLook/AppDelegate.swift
+++ AirLook/AppDelegate.swift
@@ -9,38 +9,87 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,WeiboSDKDelegate {
+
+    
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        WeiboSDK.registerApp("3161059495")
+        NotificationCenter.default.addObserver(self, selector: #selector(onRecviceSINA_CODE_Notification(notification:)), name: NSNotification.Name(rawValue: "SINA_CODE"), object: nil)
+
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    //回调
+    func didReceiveWeiboResponse(_ response: WBBaseResponse!) {
+        if response.isKind(of: WBAuthorizeResponse.self){
+            if (response.statusCode == WeiboSDKResponseStatusCode.success) {
+                let authorizeResponse : WBAuthorizeResponse = response as! WBAuthorizeResponse
+                let userID = authorizeResponse.userID
+                let accessToken = authorizeResponse.accessToken
+                print("userID:\(String(describing: userID))\naccessToken:\(String(describing: accessToken))")
+                let userInfo = response.userInfo as Dictionary
+                
+                /*
+                 userID:Optional("2115672863")
+                 accessToken:Optional("2.00rJJL_CZyTv8D5fd60ecacaZ5OGrB")
+                 **/
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "SINA_CODE"), object: nil, userInfo:userInfo )
+            }
+        }
+    }
+    
+    func didReceiveWeiboRequest(_ request: WBBaseRequest!) {
+        print(request)
+    }
+    
+    //第二步  通过通知得到登录后获取的用户信息
+    @objc func onRecviceSINA_CODE_Notification(notification:NSNotification)
+    {
+//        SVProgressHUD.showSuccessWithStatus("获取到用户信息", duration: 1)
+        var userinfoDic : Dictionary = notification.userInfo!
+        
+        print("userInfo:\(userinfoDic)")
+        
+        /*
+         userInfo:[AnyHashable("refresh_token"): 2.00rJJL_CZyTv8D05b152f6567LZcJC,
+         AnyHashable("app"): {
+         logo = "http://timg.sjs.sinajs.cn/miniblog2style/images/developer/default_50.gif";
+         name = "\U672a\U901a\U8fc7\U5ba1\U6838\U5e94\U7528";
+         },
+         AnyHashable("access_token"): 2.00rJJL_CZyTv8D5fd60ecacaZ5OGrB,
+         AnyHashable("isRealName")  : true,
+         AnyHashable("remind_in")   : 157679999,
+         AnyHashable("scope")       : follow_app_official_microblog,
+         AnyHashable("uid")         : 2115672863,
+         AnyHashable("expires_in")  : 157679999]
+         */
+        let userAppInfo: Dictionary<String,String> = userinfoDic["app"] as! Dictionary
+        refeshUserInfo(dic: userAppInfo as NSDictionary)
+    }
+    
+    //第三步 刷新用户界面
+    func refeshUserInfo(dic : NSDictionary){
+        let headimgurl: String = dic["logo"] as! String
+        let nickname: String = dic["name"] as! String
+        print(headimgurl)
+        print(nickname)
+//        self.headerImg.sd_setImageWithURL(NSURL(string: headimgurl))
+//        self.nicknameLbl.text = nickname
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+         return WeiboSDK.handleOpen(url, delegate: self)
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+         return WeiboSDK.handleOpen(url, delegate: self)
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
--- AirLook/ViewController.swift
+++ AirLook/ViewController.swift
@@ -11,8 +11,9 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    var weibo:[UIView]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +25,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
+        let scene = SCNScene()
         // Set the scene to the view
         sceneView.scene = scene
+        //去锯齿
+        sceneView.antialiasingMode = SCNAntialiasingMode.multisampling4X
+        //
+        addWeiBoSence()
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +41,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -51,30 +57,72 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+    
+    
+    func addWeiBoSence(){
+        let maxCross = 5
+        let maxLine  = 5
+        let nodeSizeW = 0.4
+        let nodeSizeH = 0.3
+//        let nodeMargin = 0.1
+        
+        let nodeAreaW:Float = Float(nodeSizeW/* + nodeMargin*/)
+        let nodeAreaH:Float = Float(nodeSizeH/* + nodeMargin*/)
+        
+        
+        
+        
+        let sp = SCNSphere(radius: 0.02)
+//        sp.firstMaterial?.diffuse.contents = UIColor.
+        let rootNode = SCNNode(geometry: sp)
+        rootNode.position = SCNVector3Make(0, 0, 0)
+        sceneView.scene.rootNode.addChildNode(rootNode)
+        
+        
+        
+        
+        for index in 0..<(maxCross*maxLine) {
+            let weiBoBox = SCNBox(width: CGFloat(nodeSizeW), height: CGFloat(nodeSizeH), length: 0.02, chamferRadius: 0.02)
+            let weiBoNode = SCNNode(geometry: weiBoBox)
+            let cross:Float = Float(index/maxCross)
+            let line:Float = Float(index%maxLine)
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+            let y:Float = nodeAreaH * 2 - (line*nodeAreaH) 
+            let z:Float = -1
+            weiBoNode.position = SCNVector3Make(0,y,z)
+            let emptyNode = SCNNode()
+            emptyNode.position = SCNVector3Zero
+            emptyNode.rotation = SCNVector4Make(1, 0, 0, -(line-2)*0.15)
+            let action = SCNAction.rotateBy(x: 0, y: CGFloat(-(cross-2)*0.6), z: 0, duration: 0)
+            emptyNode.runAction(action)
+            emptyNode.addChildNode(weiBoNode)
+            rootNode.addChildNode(emptyNode)
+            
+
+            let contentImage = WeiBoImage.image(text: "一二三四五六七八九十十一十二十三十四十五十六十七十八十九二十")
+            let image = UIImage(named: "whiteImage")
+            
+            let images = [contentImage,image,image,image,image,image]
+            
+            var materials:[SCNMaterial] = []
+            for index in 0..<6 {
+                let material = SCNMaterial()
+                material.multiply.contents = images[index]
+                materials.append(material)
+            }
+            weiBoBox.materials = materials
+        }
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+          loginWeiBo()
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    //登陆
+    func loginWeiBo(){
+        let request : WBAuthorizeRequest = WBAuthorizeRequest.request() as! WBAuthorizeRequest
+        request.redirectURI = "https://github.com/SherlockQi"
+        request.scope = "all"
+//        request.userInfo = ["SSO_Key":"SSO_Value"]
+        WeiboSDK.send(request)
     }
 }
+//149023EB-E6BB-464B-87F2-73EE1C233EB8
